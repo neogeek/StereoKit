a0d956166fa41bd704413d25a3ef76b96b2d1723
Scott Doxey <neogeek0101@gmail.com>
2020-07-22 13:13:16 -0400

    Store x and y variables.

--- StereoKit/Systems/Camera.cs
+++ StereoKit/Systems/Camera.cs
@@ -10,15 +10,15 @@
 
             var point = mat * new Vec4(position.x, position.y, position.z, 1);
 
-            point.x = (point.x / point.w + 1f) * 0.5f * StereoKitApp.System.displayWidth;
-            point.y = (point.y / point.w + 1f) * 0.5f * StereoKitApp.System.displayHeight;
+            var x = (point.x / point.w + 1f) * 0.5f * StereoKitApp.System.displayWidth;
+            var y = (point.y / point.w + 1f) * 0.5f * StereoKitApp.System.displayHeight;
 
             if (point.w == 0)
             {
                 return Vec3.Zero;
             }
 
-            return new Vec3(point.x, point.y, position.z);
+            return new Vec3(x, y, position.z);
         }
     }
 }
